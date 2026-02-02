ab0b4d9370e0f4c32dd5e849750a3aeedb93913a
david <sarigoo421@gmail.com.il>
2026-02-02 12:19:26 +0200

    update simple

--- simple_operations.py
+++ simple_operations.py
@@ -1,8 +1,31 @@
+# add function add b to a
 def add(a,b):
-    return a+b
+    try:
+        return a+b
+    except:
+        print("the values are not numbers")
+# sub function sub b to a
 def subtract(a,b):
-    return a-b
+    try:
+        return a - b
+    except:
+        print("the values are not numbers")
+
+# multiplay fun multiplay b to a
 def multiply(a,b):
-    return a*b
+    try:
+        return a*b
+    except:
+        print("the values are not numbers")
+
+# divide function divide a to b
 def divide(a,b):
-    return a/b
+    try:
+        return a/b
+    except ZeroDivisionError:
+        print("you cannot divide by zero")
+
+    except TypeError:
+        print("the values are not numbers")
+
+
